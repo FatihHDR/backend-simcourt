358871f44f55aa7b5a3844df962e94e943180fc5
fatihhdr <fatahillah.alt@gmail.com>
2025-04-15 18:47:42 +0700

    Add directive to ensure asset URLs use HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+    
+        // Ensure all asset URLs use HTTPS
+        \Illuminate\Support\Facades\Blade::directive('secure_asset', function ($expression) {
+            return "<?php echo asset($expression, true); ?>";
+        });
     }
 }
