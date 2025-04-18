9205ff175f9af3b524705ecf8ac704331622782f
fatihhdr <fatahillah.alt@gmail.com>
2025-04-18 16:11:34 +0700

    Revert application URL in config/app.php to use localhost for local development

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://ecourt.umm.ac.id'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
